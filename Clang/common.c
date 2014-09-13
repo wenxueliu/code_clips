b6be10dfca06ace22bcd9ede07a5a2c82f942bd3
wenxueliu <wenxueliu73@gmail.com>
2014-09-13 21:43:35 +0800

    add some c code for common

---
+++ Clang/common.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#define     MAXARGC     50
+#define     WHITE       " \t\n"
+int buf_args(char *buf, int(*optfunc)(int, char**))
+{
+    char    *ptr,   *argv[MAXARGC];
+    int     argc;
+
+    if (strtok(buf, WHITE) == NULL)
+    {
+        return -1;
+    }
+    argv[argc = 0] = buf;
+    while((ptr = strtok(NULL, WHITE)) != NULL)
+    {
+        if (++argc >= MAXARGC - 1)
+        {
+            return -1;
+        }
+        argv[argc] = ptr;
+    }
+    argv[++argc] = NULL;
+    return ((*optfunc)(argc, argv));
+}
