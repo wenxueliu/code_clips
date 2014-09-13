d2f1b7c62e2f0f44a736146a9cf613803e1b9aaa
wenxueliu <wenxueliu73@gmail.com>
2014-09-13 21:44:04 +0800

    add color use for color ouput

---
+++ shell/color.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\e[30mERROR when compile \e[m"
+echo -e "\e[31mERROR when compile \e[m"
+echo -e "\e[32mERROR when compile \e[m"
+echo -e "\e[33mERROR when compile \e[m"
+echo -e "\e[34mERROR when compile \e[m"
+echo -e "\e[35mERROR when compile \e[m"
+echo -e "\e[36mERROR when compile \e[m"
+echo -e "\e[37mERROR when compile \e[m"
+echo -e "\e[38mERROR when compile \e[m"
+echo -e "\e[39mERROR when compile \e[m"
