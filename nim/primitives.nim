b6a8d1265ef4188fa18873571fbbc1103394eee9
wenxueliu <wenxueliu73@gmail.com>
2015-05-12 17:27:24 +0800

    update nim primitive

--- nim/primitives.nim
+++ nim/primitives.nim
@@ -8,10 +8,27 @@ var n : int = 3
 echo("n = 3 to char : ", chr(n))
 
 #String initialize
+echo "========= StringTest"
 var str : string  = "abc"
 str = str & "cde"
 #str = add(str, "fg")
 echo ("str : ", str)
+echo """
+<html>
+  <head>
+  </head>\n\n
+
+  <body>
+  </body>
+</html> """
+
+proc re(s:string) : string = s
+
+echo r".""."
+echo re"\b[a-z]++\n"
+echo "\b[a-z]++\b\n"
+echo re("\b[a-z]++\b")
+
 
 var str1 : string = "" # on the heap
 var str2 : string      # str2 = nil, trade-off str1
@@ -24,6 +41,7 @@ var str2 : string      # str2 = nil, trade-off str1
 
 block Integer:
   # Immutable variables
+  echo "============= IntegerTest"
   let
     a = 0     # int
     b = 0'i8  # int8
@@ -121,50 +139,117 @@ block AdvanceType:
 
   echo 2 + MyInteger(4)
 
-#====== module
+block EnumsTest:
+    echo "======= EnumsTest "
+    type
+        CompassDirections = enum
+            cdNorth, cdEast, cdSouth, cdWest
+        Colors {. pure .} = enum
+            Red = "FF0000", Green = (1, "00FF00"), Blue = "0000FF"
 
-type
-    Animal* = object
-        name *, special* : string
-        age : int
+        Signals = enum
+            sigQuit = 3, sigAbort = 6, sigKill = 9
 
-proc sleep*(a: var Animal) =
-    a.age += 1
+    for direction in ord(low(CompassDirections))..ord(high(CompassDirections)) :
+        echo CompassDirections(direction), "ord: ", direction
+        echo CompassDirections(direction), "ord: ", direction
 
-proc sleep*(a: ref Animal) =
-    a.age += 1
+    for col in low(Colors)..high(Colors):
+        echo Colors(col), ": ", col
+        echo Colors(ord(col)), "ord : ", ord(col)
 
-proc dead*(a: Animal) : bool =
-    result = a.age > 28
+    for sig in low(Signals)..high(Signals):
+        echo Signals(sig), ": ", sig
+        echo Signals(ord(sig)), "ord : ", ord(sig)
 
-var carl : Animal
-carl = Animal(name : "Carl",
-                special : "L. gramma",
-                age : 19)
+block distinctTest:
+    echo "=========== distinctTest"
+    type
+        Dollars  = distinct float
+    var a = 20.Dollars
+    #a = 25 # Doesn't compile
+    a = 25.Dollars
+    echo repr(a)
 
-let joe = Animal(name : "Joe",
-                special : "H. sapiens",
-                age : 29)
+    proc `*` (a,b : Dollars) : Dollars {.borrow.}
+    proc `+` (a,b : Dollars) : Dollars {.borrow.}
+    a = 20.Dollars * 5.Dollars
+    echo repr(a)
 
-echo "carl.dead: ", carl.dead
-echo "joe.dead: ", joe.dead
-for i in 0..10:
-    carl.sleep()
-echo "carl.dead: ", carl.dead
-echo "joe.dead: ", joe.dead
+    type
+        Foo = object
+            a: int
+        MyFoo {.borrow: `.`.} = distinct Foo
 
-let mittens : ref Animal = new (Animal)
-mittens.name = "mittens"
-mittens.special = "P. leo"
-mittens.age = 6
+    var value1 : Foo
+    value1 = Foo(a:6)
+    echo value1.a
 
-mittens.sleep()
-echo "mittens.age: ", mittens.age
+    var value2 : MyFoo
+    echo value2.a
 
-type
-    PAnmial* = ref Animal
-let spot = PAnmial(name : "Spot",
-                special : "C. lups",
-                age : 1)
+block ArrayTest:
+    echo "=============  ArrayTest"
+    type
+        ThreeStringAddress = array[3, string]
+    let names : ThreeStringAddress = ["jasmine",  "Ktisztina", "Kristof"]
+    let addresses: ThreeStringAddress = ["101 Betburweg", "66 Bellion Drive", "194 Laarderweg"]
 
+    proc zip[I, T](a, b : array[I, T]):
+        array[I, tuple[a,b:T]] =
+        for i in low(a)..high(a):
+            result[i] = (a[i],b[i])
+
+    let nameAndAddrees = zip(names,addresses)
+
+    type
+        PartsOfSpeech {.pure.} = enum
+            Pronoun, Verb, Article, Adjective, Noun, Adverb
+    let partOfSpeechExamples: array[PartsOfSpeech, string] = [
+        "he", "reads", "the", "green", "book", "slowly" ]
+    for i in low(partOfSpeechExamples)..high(partOfSpeechExamples):
+        echo i
+
+    type
+        Matrix[W, H : static[int]] =
+            array[1..W, array[1..H, int]]
+
+    let mat1: Matrix[2,2] = [[1,0], [0,1]]
+    let mat2: Matrix[2,2] = [[0,1], [1,0]]
+
+    proc `+`[W, H](a, b:Matrix[W,H]):
+                Matrix[W, H] =
+        for i in 1..high(a):
+            for j in 1..high(a[0]):
+                result[i][j] = a[i][j] + b[i][j]
+    # See `$` from the procs section
+    # TODO compile failure
+    # echo mat1 + mat2
+
+block SeqTest:
+    var
+        a = @[1,2,3]
+        b = newSeq[int](3)
+
+    for i,v in a:
+        b[i] = v * v
+
+    for i in 4..6:
+        b.add(i*i)
+
+    b.delete(0)
+    b = a[0] & b
+
+    let x = @[1,2,3]
+    x.add(4)
+
+    var y = @[1,2,3]
+    y.add(4)
+
+    proc foo(mySeq: var seq[int]) =
+        mySeq[9] = 999
+
+    var thisSeq = newSeq[int](10)
+    foo(thisSeq)
+    assert thisSeq[9] = 999
 
