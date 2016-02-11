df568246c5db21068fb3cb5f3977fafafd6e0376
wenxueliu <wenxueliu73@gmail.com>
2016-02-11 22:19:34 +0800

    add asm dir

---
+++ asm/baby-steps-in-assembly/step1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Declaration */
+void simplest_function();
+
+int main() {
+	printf("[.] Calling simplest_function()\n");
+	simplest_function();
+	printf("[.] It worked!\n");
+	return 0;
+}
