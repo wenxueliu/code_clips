e577ecc89072c801187796dc5afd29660eec5749
wenxueliu <wenxueliu73@gmail.com>
2017-02-14 11:53:13 +0800

    update Clang/macro.c

--- Clang/macro.c
+++ Clang/macro.c
@@ -1,4 +1,4 @@
-
+// 更多参考 include/linux/kernel.h
 
 #ifdef DISABLE_BRANCH_PREDICTION
 #  define LIKELY_IS(x,y) (x)
@@ -42,7 +42,25 @@
 #define check_type_match(expr1, expr2)   \
     ((typeof(expr1) *)0 != (typeof(expr2) *)0)
 
+/*
+ * 1. (e): Compute expression e.
+ * 2. !!(e): Logically negate twice: 0 if e == 0; otherwise 1.
+ * 3. -!!(e): Numerically negate the expression from step 2: 0 if it was 0; otherwise -1.
+ * 4. struct{int: -!!(0);} --> struct{int: 0;}: If it was zero, then we declare a struct
+ *    with an anonymous integer bitfield that has width zero. Everything is fine and we
+ *    proceed as normal.
+ * 5. struct{int: -!!(1);} --> struct{int: -1;}: On the other hand, if it isn't zero,
+ *    then it will be some negative number. Declaring any bitfield with negative width
+ *    is a compilation error.
+ */
 #define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)])) : reference:https://scaryreasoner.wordpress.com/2009/02/28/checking-sizeof-at-compile-time/)
+
+http://stackoverflow.com/questions/9229601/what-is-in-c-code?rq=1
+#define BUILD_BUG_ON(condition) ((void)BUILD_BUG_ON_ZERO(condition))
+#define MAYBE_BUILD_BUG_ON(cond) ((void)sizeof(char[1 - 2 * !!(cond)]))
+#define BUILD_BUG_ON_ZERO(e) (sizeof(struct { int:-!!(e); }))
+#define BUILD_BUG_ON_NULL(e) ((void *)sizeof(struct { int:-!!(e); }))
+
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
 
@@ -56,7 +74,33 @@
 #define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
 #define round_down(x, y) ((x) & ~__round_mask(x, y))
 
+#define rounddown(x, y) (				\
+{							\
+	typeof(x) __x = (x);				\
+	__x - (__x % (y));				\
+}							\
+)
+
 #define FIELD_SIZEOF(t, f) (sizeof(((t*)0)->f))
 #define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
 #define DIV_ROUND_UP_ULL(ll,d) \
         ({ unsigned long long _tmp = (ll)+(d)-1; do_div(_tmp, d); _tmp; })
+
+
+/* Casts 'pointer' to 'type' and issues a compiler warning if the cast changes
+ * anything other than an outermost "const" or "volatile" qualifier.
+ *
+ * The cast to int is present only to suppress an "expression using sizeof
+ * bool" warning from "sparse" (see
+ * http://permalink.gmane.org/gmane.comp.parsers.sparse/2967). */
+#define CONST_CAST(TYPE, POINTER)                               \
+    ((void) sizeof ((int) ((POINTER) == (TYPE) (POINTER))),     \
+     (TYPE) (POINTER))
+
+struct recirc_id_node {
+    int field1;
+};
+
+void recirc_id_node_try_ref_rcu(const struct recirc_id_node *n_) {
+    struct recirc_id_node *node = CONST_CAST(struct recirc_id_node *, n_);
+}
